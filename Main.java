4ce1ce5dae958f26bea442cff04f28a321946460
Ardra-14 <144412428+Ardra-14@users.noreply.github.com>
2024-04-03 20:34:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+        int a = 5 ;
+        int b =38 ;
+        int diff = b-a;
+
+        if(a>b){
+            System.out.println(a + " is greater than " + b + " by " +diff+ " units");
+        }else{
+            System.out.println(b + " is greater than " + a + " by " +diff+ " units");
+        }
+
+    }
+}
